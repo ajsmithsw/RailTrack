cc48a7ab2b1a58c3e569b2ba7dc6158380e15425
Alexander Smith <alex@duriancode.com>
2017-04-21 21:20:00 +0100

    Added macOS project to Forms solution

---
+++ RailTrack.macOS/Main.cs
@@ -0,0 +1,16 @@
+﻿using AppKit;
+
+namespace RailTrack.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+
+			NSApplication.Main(args);
+		}
+	}
+}
---
+++ RailTrack/RailTrackPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RailTrack
+{
+	public partial class RailTrackPage : ContentPage
+	{
+		public RailTrackPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
